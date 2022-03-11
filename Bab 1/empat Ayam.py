6ae09404362930450695cf24fcb06ce571502e7c
Rahdriawan Prismana <63779468+Rahdriawan@users.noreply.github.com>
2022-03-11 15:34:24 +0700

    Add files via upload

---
+++ Bab 1/empat Ayam.py
@@ -0,0 +1,5 @@
+def rerata(x):
+    hasil = sum(x) / len(x)
+    return hasil
+
+print(rerata([3,5,7,5]))
---
+++ Bab 1/satu Harimau.py
@@ -0,0 +1,8 @@
+def cetakSiku():
+    baris = 5
+    for i in range(baris + 1):
+        for j in range(i):
+            print("*", end="")  #end berfungsi untuk menambahkan string tambahan di akhir string
+        print("*")
+
+cetakSiku()
