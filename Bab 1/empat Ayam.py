df9df3faf0ed7d9d7ac39741c90a100c72876d05
Rahdriawan Prismana <63779468+Rahdriawan@users.noreply.github.com>
2022-03-16 22:50:25 +0700

    Delete Bab 1 directory

--- Bab 1/empat Ayam.py
+++
@@ -1,5 +0,0 @@
-def rerata(x):
-    hasil = sum(x) / len(x)
-    return hasil
-
-print(rerata([3,5,7,5]))
--- Bab 1/satu Harimau.py
+++
@@ -1,8 +0,0 @@
-def cetakSiku():
-    baris = 5
-    for i in range(baris + 1):
-        for j in range(i):
-            print("*", end="")  #end berfungsi untuk menambahkan string tambahan di akhir string
-        print("*")
-
-cetakSiku()
